b07cfc9bb4e5f0aec8d366b57f58284f8a4dd2ea
José Luiz <Jos Luiz@DESKTOP-JD6IUD9>
2022-11-29 11:08:41 -0300

    finalização metodo resgatar lista de usuarios

--- src/com/biblioteca/model/dao/UsuarioDao.java
+++ src/com/biblioteca/model/dao/UsuarioDao.java
@@ -3,6 +3,7 @@ package com.biblioteca.model.dao;
 import com.biblioteca.connection.ConnectionMySQL;
 import com.biblioteca.controller.IUsuario;
 import com.biblioteca.model.UsuarioModel;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import javax.swing.JOptionPane;
 
@@ -10,18 +11,15 @@ import javax.swing.JOptionPane;
  *
  * @author Instrutores
  */
-public class UsuarioDao extends ConnectionMySQL implements IUsuario{
+public class UsuarioDao extends ConnectionMySQL implements IUsuario {
 
     @Override
     public boolean salvarUsuarioDAO(UsuarioModel pUsuarioModel) {
         try {
             this.conectar();
-          return this.executarInsertUpdateSQL(
-                  "INSERT INTO tbl_usuario "
-                          + "(nome_usuario, senha_usuario, login_usuario, nivel_usuario) "
-                          + "VALUES('"+pUsuarioModel.getNomeUsuario()+"','"+pUsuarioModel.getSenhaUsuario()+"','"
-                          +pUsuarioModel.getLoginUsuario()+"','"+pUsuarioModel.getNivelAcessoUsuario()+"')"
-          );
+            return this.executarInsertUpdateSQL(
+                    "INSERT INTO tbl_usuario (nome_usuario, senha_usuario, login_usuario, nivel_usuario) VALUES('" + pUsuarioModel.getNomeUsuario() + "','" + pUsuarioModel.getSenhaUsuario() + "','" + pUsuarioModel.getLoginUsuario() + "','" + pUsuarioModel.getNivelAcessoUsuario() + "')"
+            );
         } catch (Exception e) {
             JOptionPane.showMessageDialog(null, "Erro ao salvar o usuário",
                     "Atenção", JOptionPane.ERROR_MESSAGE);
@@ -33,18 +31,76 @@ public class UsuarioDao extends ConnectionMySQL implements IUsuario{
 
     @Override
     public UsuarioModel getUsuarioDAO(int pIdUsuario) {
-    UsuarioModel usuarioModel = new UsuarioModel();
-    return usuarioModel;
+        UsuarioModel usuarioModel = new UsuarioModel();
+        try {
+            this.conectar();
+            this.executarSQL("SELECT * FROM tbl_usuario WHERE pk_id_usuario='" + pIdUsuario + "';");
+
+            while (this.getResultSet().next()) {
+                usuarioModel.setIdUsuario(this.getResultSet().getInt(1));
+                usuarioModel.setNomeUsuario(this.getResultSet().getString(2));
+                usuarioModel.setSenhaUsuario(this.getResultSet().getString(3));
+                usuarioModel.setLoginUsuario(this.getResultSet().getString(4));
+                usuarioModel.setNivelAcessoUsuario(this.getResultSet().getString(5));
+            }
+
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Erro ao buscar o usuário",
+                    "Atenção", JOptionPane.ERROR_MESSAGE);
+        } finally {
+            this.fecharConexao();
+        }
+        return usuarioModel;
     }
 
     @Override
     public UsuarioModel getUsuarioDAO(String pLogin) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        UsuarioModel usuarioModel = new UsuarioModel();
+        try {
+            this.conectar();
+            this.executarSQL("SELECT * FROM tbl_usuario WHERE login_usuario='" + pLogin + "';");
+
+            while (this.getResultSet().next()) {
+                usuarioModel.setIdUsuario(this.getResultSet().getInt(1));
+                usuarioModel.setNomeUsuario(this.getResultSet().getString(2));
+                usuarioModel.setSenhaUsuario(this.getResultSet().getString(3));
+                usuarioModel.setLoginUsuario(this.getResultSet().getString(4));
+                usuarioModel.setNivelAcessoUsuario(this.getResultSet().getString(5));
+            }
+
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Erro ao buscar o usuário",
+                    "Atenção", JOptionPane.ERROR_MESSAGE);
+        } finally {
+            this.fecharConexao();
+        }
+        return usuarioModel;
     }
 
     @Override
-    public ArrayList<UsuarioModel> getListaUsuarioDAO() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public ArrayList<UsuarioModel> getListaUsuarioDAO(){
+        ArrayList<UsuarioModel> listaUsuarioModel = new ArrayList<>();
+        UsuarioModel usuarioModel = new UsuarioModel();
+        try {
+            this.conectar();
+            this.executarSQL("SELECT * FROM tbl_usuario;");
+
+            while (this.getResultSet().next()) {
+                usuarioModel = new UsuarioModel();
+                usuarioModel.setIdUsuario(this.getResultSet().getInt(1));
+                usuarioModel.setNomeUsuario(this.getResultSet().getString(2));
+                usuarioModel.setSenhaUsuario(this.getResultSet().getString(3));
+                usuarioModel.setLoginUsuario(this.getResultSet().getString(4));
+                usuarioModel.setNivelAcessoUsuario(this.getResultSet().getString(5));
+                listaUsuarioModel.add(usuarioModel);
+            }
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Erro ao buscar o usuários",
+                    "Atenção", JOptionPane.ERROR_MESSAGE);
+        } finally {
+            this.fecharConexao();
+        }
+        return listaUsuarioModel;
     }
 
     @Override
@@ -67,5 +123,4 @@ public class UsuarioDao extends ConnectionMySQL implements IUsuario{
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
-   
 }

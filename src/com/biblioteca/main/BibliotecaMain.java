5549b226a8dd5dc695485aceda55a10fbcb51acc
joseluiz <joseluiz@fedora>
2023-08-27 00:08:12 -0300

    inclusão da opção de venda e preço na tela de cadastr de livros, e outros pequenos ajustes.

--- src/com/biblioteca/main/BibliotecaMain.java
+++ src/com/biblioteca/main/BibliotecaMain.java
@@ -20,26 +20,28 @@ public class BibliotecaMain {
      */
     public static void main(String[] args) throws SQLException {
 
-        SessaoUsuarioModel.codigoUsuario = 1;
-        SessaoUsuarioModel.loginUsuario = "luiz";
-        SessaoUsuarioModel.nivelAcesso = "ADMINISTRADOR";
-        SessaoUsuarioModel.nomeUsuario = "José Luiz do Monte Gomes";
-        SessaoUsuarioModel.instituicao = "FECMA";
-        new FrmTelaPrincipal().setVisible(true);
+//        SessaoUsuarioModel.codigoUsuario = 1;
+//        SessaoUsuarioModel.loginUsuario = "luiz";
+//        SessaoUsuarioModel.nivelAcesso = "ADMINISTRADOR";
+//        SessaoUsuarioModel.nomeUsuario = "José Luiz do Monte Gomes";
+//        SessaoUsuarioModel.instituicao = "FECMA";
+//        new FrmTelaPrincipal().setVisible(true);
 //        new FrmLogin(null, true).setVisible(true);
-//        new SplashView().setVisible(true);
-//        for (int i = 0; i <= 50; i++) {
-//            try {
-//                Thread.sleep(30);
-//            } catch (InterruptedException ex) {
-//            }
-//            if (i == 50) {
-//
-//                FrmLogin loginView = new FrmLogin();
-//                Splash.setVisible(false);
-//                loginView.setVisible(true);
-//            }
-//        }
+        SplashView splashView = new SplashView();
+
+        splashView.setVisible(true);
+        for (int i = 0; i <= 50; i++) {
+            try {
+                Thread.sleep(30);
+            } catch (InterruptedException ex) {
+            }
+            if (i == 50) {
+
+                FrmLogin frmLogin = new FrmLogin(null, true);
+                splashView.setVisible(false);
+                frmLogin.setVisible(true);
+            }
+        }
     }
 
 }
